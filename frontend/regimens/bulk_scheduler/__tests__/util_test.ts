a6ff90f189db9e25cb4bcd03523598bd0ced065d
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-01-30 23:57:03 +0100

    asd

--- frontend/regimens/bulk_scheduler/__tests__/util_test.ts
+++
@@ -1,8 +0,0 @@
-import { msToTime } from "../utils";
-
-describe("msToTime", () => {
-  it("handles bad inputs", () => {
-    // tslint:disable-next-line:no-any
-    expect(msToTime("0" as any)).toBe("00:01");
-  });
-});
